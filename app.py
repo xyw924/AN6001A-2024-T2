7f2ecbf72ecd36df92352aaf9627b97aa82b42ae
xyw924 <100886012+xyw924@users.noreply.github.com>
2024-11-11 03:58:54 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"]) # 通过 @，Flask 识别出 index() 函数应该在用户访问根路径时执行
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__": # 第二次确认是main
+    app.run()
+
+
+
